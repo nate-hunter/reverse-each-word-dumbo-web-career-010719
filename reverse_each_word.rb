38a5159f04143aa68176efcfa8e5a98668ff319b
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-24 01:18:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word (sentence)
-  sentence.join(" ")
-  sentence.each do |word|
-    word.reverse
-    
-  end 
+  #sentence.join(" ")
+  sentence.split.each { |word| 
+    word.reverse}.join(" ")
 end
 
