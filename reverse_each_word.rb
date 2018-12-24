d8bad6c10a63ded2f9bf2cc5f3c915d56e365d93
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-24 01:19:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,12 @@ def reverse_each_word (sentence)
     word.reverse}.join(" ")
 end
 
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
